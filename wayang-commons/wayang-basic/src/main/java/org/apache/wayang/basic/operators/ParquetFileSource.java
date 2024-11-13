13683466bc4a3c425e123edb0b38097c5f6206ec
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2024-11-13 21:17:05 +0100

    Change to TableSource

--- wayang-commons/wayang-basic/src/main/java/org/apache/wayang/basic/operators/ParquetFileSource.java
+++ wayang-commons/wayang-basic/src/main/java/org/apache/wayang/basic/operators/ParquetFileSource.java
@@ -1,30 +1,15 @@
 package org.apache.wayang.basic.operators;
 
-import org.apache.wayang.basic.data.Record;
-import org.apache.wayang.core.plan.wayangplan.UnarySource;
-import org.apache.wayang.core.types.DataSetType;
-import org.apache.wayang.basic.types.RecordType;
-
-public class ParquetFileSource extends UnarySource<Record> {
+public class ParquetFileSource extends TableSource {
     private final String filepath;
 
     public ParquetFileSource(String filepath, String ...columnNames) {
-       this(filepath, createOutputDataSetType(columnNames)); 
-    }
-
-    private ParquetFileSource(String filepath, DataSetType<Record> type) {
-        super(type);
+        super(filepath, columnNames); 
         this.filepath = filepath;
     }
 
     public String getFilePath() {
 
         return this.filepath;
-    }
-
-    private static DataSetType<Record> createOutputDataSetType(String[] columnNames) {
-        return columnNames.length == 0 ?
-                DataSetType.createDefault(Record.class) :
-                DataSetType.createDefault(new RecordType(columnNames));
     }
 }
